9981fb34a1e85cdb78703e4dd70e9abee61a76af
Joao-Mendes-DEV <joaomfmendes@hotmail.com.br>
2025-08-21 14:26:04 -0300

    Create super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+int main (){
+
+    char estado1, estado2, cidade1[50], cidade2[50], cod1[50], cod2[50];
+    int populacao1, populacao2, pontosTuristicos1, pontosTuristicos2;
+    float area1, area2, pib1, pib2;
+
+    printf("Digite os Dados da Carta 1:\n");
+    printf("Estado: ");
+    scanf("%c", &estado1);
+
+    printf("Código: ");
+    scanf("%s", cod1);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", cidade1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos1);
+
+    printf("\nDigite os Dados da Carta 2:\n");
+    printf("Estado: ");
+    scanf(" %c", &estado2);
+
+    printf("Código: ");
+    scanf("%s", cod2);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", cidade2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área: ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos2);
+
+    printf("\nCarta 1: \n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", cod1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f Km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+
+    printf("\nCarta 2: \n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", cod2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f Km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+
+    return 0;
+}
